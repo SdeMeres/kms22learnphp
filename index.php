8df0adce0d8da6ec2a0aa606deb7c1d87f5af1e7
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-10-14 10:07:01 +0300

    Add functions

--- index.php
+++ index.php
@@ -1,28 +1,23 @@
 <?php
 
-$array = ['adf', 12, true, 5];
-var_dump($array);
-var_dump($array[0]);
-$array[1] = 55;
-var_dump($array);
-$array[] = 'hello';
-var_dump($array);
-$array = [
-    'name' => 'Kaspar',
-    'age' => 30,
-    5 => 1,
-    2,
-    3
-];
-var_dump($array);
-$array = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-];
-var_dump($array);
-var_dump($array[1][2]);
-$array = [1, 2, 3];
-$array2 = [4, 5, 6, 7];
-$array3 = $array + $array2;
-var_dump($array3);
+$obj = new stdClass();
+$obj->name = 'Kaspar';
+$obj->age = 30;
+var_dump($obj);
+
+function hello(){
+    echo "hello world";
+}
+
+hello();
+hello();
+var_dump(hello());
+
+function sum($a, $b){
+    return $a+$b;
+    echo 'akjsdasd';
+}
+
+$answer = sum(1,2);
+var_dump($answer);
+var_dump(sum(3,5));
