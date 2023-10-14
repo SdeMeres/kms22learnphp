ba4315be81111e2677563b511658c9522496ae6c
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-10-14 10:44:03 +0300

    Add if and loops examples

--- index.php
+++ index.php
@@ -1,23 +1,98 @@
 <?php
-
-$obj = new stdClass();
-$obj->name = 'Kaspar';
-$obj->age = 30;
-var_dump($obj);
-
-function hello(){
-    echo "hello world";
+$num = 2;
+if($num > 10){
+    var_dump('suurem');
+} else if($num === 10) {
+    var_dump('võrdne');
+} else {
+    var_dump('väiksem');
 }
 
-hello();
-hello();
-var_dump(hello());
-
-function sum($a, $b){
-    return $a+$b;
-    echo 'akjsdasd';
+if($num === 1){
+    echo 'üks';
+}
+if($num === 2){
+    echo 'kaks';
+}
+if($num === 3){
+    echo 'kolm';
+}
+if($num === 4){
+    echo 'neli';
 }
 
-$answer = sum(1,2);
-var_dump($answer);
-var_dump(sum(3,5));
+switch($num){
+    case 1:
+        echo 'üks';
+        break;
+    case 6:
+    case 5:
+    case 2:
+        echo 'kaks';
+        break;
+    
+    case 3:
+        echo 'kolm';
+    case 4:
+        echo 'neli';
+        break;
+    default:
+        echo 'midagi muud';
+}
+
+for($i=0;$i<10;$i++){
+    var_dump($i);
+}
+
+for($i=5;$i<10;$i++){
+    var_dump($i);
+}
+for($i=10;$i<10;$i++){
+    var_dump($i);
+}
+
+for($i=10;$i>0;$i--){
+    var_dump($i);
+}
+
+for($i=1;$i<100;$i+=5){
+    var_dump($i);
+}
+for($i=1;$i<1_000_000;$i*=2){
+    var_dump($i);
+}
+
+// for($i=1;$i<10;$i--){
+//     var_dump($i);
+// }
+
+// for(;;){
+//     var_dump('hello');
+// }
+
+$date = time();
+while(time() < $date+1){
+    var_dump('hello');
+}
+
+$i = 0;
+while($i>0){
+    var_dump('while');
+}
+
+do {
+    var_dump('do while');
+} while($i>0);
+
+$array = [1, 2, 3, 4];
+
+for($i=0;$i<count($array);$i++){
+    var_dump($array[$i]);
+}
+
+foreach($array as $value){
+    var_dump($value);
+}
+foreach($array as $key=>$value){
+    var_dump($key . ':' . $value);
+}
