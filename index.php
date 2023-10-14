f161c9053eca013701a6f184e6b05f74dd7845a3
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-10-14 13:04:07 +0300

    MagicMethods examples

--- index.php
+++ index.php
@@ -1,29 +1,43 @@
 <?php
 
 class Box {
-    public $width;
-    public static $count = 0;
-
-    public static function getCount() {
-        var_dump(static::$count);
-        
+    public function __construct($width, $height){
+        var_dump('construct', $width, $height);
     }
-    public function getWidth(){
-        var_dump(static::$count);
-        self::getCount();
-        var_dump($this->width);
+    public function __get($name){
+        var_dump($name);
+        return 'getvalue';
+    }
+    public function __set($name, $value){
+        var_dump($name, $value);
+    }
+    public function __call($name, $args){
+        var_dump($name, $args);
+    }
+    public static function __callStatic($name, $args){
+        var_dump($name, $args);
+    }
+
+    public function __invoke($num){
+        var_dump('as a function', $num);
+    }
+    public function __toString(){
+        return 'cool box';
+    }
+    public function __destruct(){
+      var_dump('destruct');  
     }
 }
-
-class MetalBox extends Box {
-    public static $count = 5;
+function makeBox(){
+    $box = new Box(1,2);
+    echo $box;
+    $box(1);
+    var_dump($box->color);
+    $box->material = 'green';
+    $box->volume(1,2,3);
+    Box::stuff(3,4,5);
+    var_dump('after $box');
 }
-
-MetalBox::getCount();
-Box::getCount();
-Box::$count = 1;
-Box::$count = 2;
-var_dump(Box::$count);
-var_dump(Box::$count);
-$box = new Box();
-$box->getWidth();
+var_dump('before func');
+makeBox();
+var_dump('after func');
