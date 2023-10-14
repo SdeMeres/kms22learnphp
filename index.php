a29d02453e192f93f2266781ee04070553dc6731
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-10-14 12:11:56 +0300

    Add inheritance and traits

--- index.php
+++ index.php
@@ -10,30 +10,40 @@ class Box {
     }
 }
 
+class MetalBox extends Box {
+    public $material = 'metal';
+}
 
+class IronBox extends MetalBox {
+    use MakesSound;
+    public $material = 'iron';
+}
+
+class WoodBox extends Box {
+    use MakesSound;
+    public $material = 'wood';
+}
+
+trait MakesSound {
+    public $sound;
+
+    public function makeSound(){
+        echo $this->sound;
+    }
+}
+
+$woodBox = new WoodBox();
+$woodBox->sound = 'kolksti';
+$woodBox->makeSound();
 
 $box = new Box();
 $box->width = 10;
 $box->height = 10;
 $box->length = 10;
-$volume = $box->volume();
-var_dump($volume);
 var_dump($box);
-$box2 = new Box();
-$box2->width = 20;
-$box2->height = 20;
-$box2->length = 20;
-$volume2 = $box2->volume();
-var_dump($volume2);
-var_dump($box2);
-var_dump($box);
-
-$box3 = clone $box;
-$box3->width = 3;
-var_dump($box3);
-var_dump($box);
-$num = 1;
-$num2 = &$num;
-$num2 = 5;
-var_dump($num);
-var_dump($num2);
+$metalBox = new MetalBox();
+$metalBox->width = 10;
+$metalBox->height = 10;
+$metalBox->length = 10;
+var_dump($metalBox);
+var_dump($metalBox->volume());
