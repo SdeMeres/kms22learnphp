9d637cc47b826103e50e4a96cfd1ec079cc1e84f
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-10-14 09:55:34 +0300

    Add arrays example

--- index.php
+++ index.php
@@ -1,30 +1,28 @@
 <?php
 
-$bool = true;
-var_dump($bool);
-$bool = false;
-var_dump($bool);
-$bool = 10 > 2;
-var_dump($bool);
-$bool = 10 < 2;
-var_dump($bool);
-$bool = 10 < 10;
-var_dump($bool);
-$bool = 10 == 10;
-var_dump($bool);
-$bool = '10' == 10;
-var_dump($bool);
-$bool = '10' === 10;
-var_dump($bool);
-$bool = 10 <= 10;
-var_dump($bool);
-$bool = 10 >= 10;
-var_dump($bool);
-$bool = '10' != 10;
-var_dump($bool);
-$bool = '10' !== 10;
-var_dump($bool);
-$null = null;
-var_dump($null);
-$var;
-var_dump($var);
+$array = ['adf', 12, true, 5];
+var_dump($array);
+var_dump($array[0]);
+$array[1] = 55;
+var_dump($array);
+$array[] = 'hello';
+var_dump($array);
+$array = [
+    'name' => 'Kaspar',
+    'age' => 30,
+    5 => 1,
+    2,
+    3
+];
+var_dump($array);
+$array = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+var_dump($array);
+var_dump($array[1][2]);
+$array = [1, 2, 3];
+$array2 = [4, 5, 6, 7];
+$array3 = $array + $array2;
+var_dump($array3);
