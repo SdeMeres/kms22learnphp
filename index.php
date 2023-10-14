7347129ab917cc69ed0cdb1e8aebe9cc11e8b2cc
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-10-14 11:54:48 +0300

    classes objects

--- index.php
+++ index.php
@@ -1,13 +1,39 @@
 <?php
-$a=0;
-$b=1;
-$sum = 0;
-while($b<4_000_000){
-    $temp = $b;
-    $b += $a;
-    $a = $temp;
-    if($b%2 === 0){
-        $sum +=$b;
+
+class Box {
+    public $width;
+    public $height;
+    public $length;
+
+    public function volume(){
+        return $this->width * $this->height * $this->length;
     }
 }
-var_dump($sum);
+
+
+
+$box = new Box();
+$box->width = 10;
+$box->height = 10;
+$box->length = 10;
+$volume = $box->volume();
+var_dump($volume);
+var_dump($box);
+$box2 = new Box();
+$box2->width = 20;
+$box2->height = 20;
+$box2->length = 20;
+$volume2 = $box2->volume();
+var_dump($volume2);
+var_dump($box2);
+var_dump($box);
+
+$box3 = clone $box;
+$box3->width = 3;
+var_dump($box3);
+var_dump($box);
+$num = 1;
+$num2 = &$num;
+$num2 = 5;
+var_dump($num);
+var_dump($num2);
